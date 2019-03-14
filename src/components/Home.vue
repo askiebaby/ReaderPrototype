49ec10d5c105f0b509c5d62f43525d42792f3c99
Askie Lin <isme408@gmail.com>
2019-03-14 17:53:51 +0800

    完成 home UI

---
+++ src/components/Home.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <div class="wholeTask">
+        <div class="taskList">
+          <div class="taskList__description">
+            <div class="taskList__description__word">文</div>
+            <div class="taskList__description__word">橫</div>
+            <div class="taskList__description__word">功</div>
+            <div class="taskList__description__word">能</div>
+            <div class="taskList__description__word">橫</div>
+            </div>
+          <div class="taskList__tasks-type1">
+            <div class="task task__completed"><span>1</span></div>
+            <div class="task"><span>2</span></div>
+            <div class="task"><span>3</span></div>
+            <div class="task"><span>4</span></div>
+            <div class="task"><span>5</span></div>
+          </div>
+          <div class="taskList__tasks-type2">
+            <div class="task task__completed">1</div>
+            <div class="task"><span>2</span></div>
+            <div class="task"><span>3</span></div>
+            <div class="task"><span>4</span></div>
+            <div class="task"><span>5</span></div>
+          </div>
+        </div>
+        <div class="taskList">
+          <div class="taskList__description">
+            <div class="taskList__description__word">文</div>
+            <div class="taskList__description__word">橫</div>
+            <div class="taskList__description__word">功</div>
+            <div class="taskList__description__word">能</div>
+            <div class="taskList__description__word">橫</div>
+            </div>
+          <div class="taskList__tasks-type1">
+            <div class="task task__completed"><span>1</span></div>
+            <div class="task"><span>2</span></div>
+            <div class="task"><span>3</span></div>
+            <div class="task"><span>4</span></div>
+            <div class="task"><span>5</span></div>
+          </div>
+          <div class="taskList__tasks-type2">
+            <div class="task task__completed">1</div>
+            <div class="task"><span>2</span></div>
+            <div class="task"><span>3</span></div>
+            <div class="task"><span>4</span></div>
+            <div class="task"><span>5</span></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.wholeTask {
+  padding: 55px 31px;
+  font-size: $contentSize;
+}
+
+%task {
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+.task {
+  @extend %task;
+
+  span {
+    line-height: 1.15em;
+  }
+}
+
+.task:nth-child(2n+2) span{
+  border-bottom: .1em solid $white;
+}
+
+.task + .task {
+  margin-left: 8px;
+}
+
+.taskList {
+  background: $gray-2;
+  color: $white;
+  border-radius: 0 30px 30px 0;
+  padding: 18px 29px 17px 10px;
+  display: flex;
+  min-height: 222px;
+
+  &__tasks-type1 {
+    .task {
+      background: $normalColor;
+    }
+  }
+
+  &__tasks-type2 {
+    .task {
+      background: $alertColor;
+    }
+  }
+
+  &__tasks-type1, &__tasks-type2 {
+    flex-grow: 1;
+    display: flex;
+
+    .task__completed {
+      background: $gray-1;
+    }
+  }
+
+  &__description {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    padding: 0 14px 0 7px;
+  }
+
+}
+
+.taskList + .taskList {
+  margin-top: 12px;
+}
+</style>
+
+
+<script>
+// @ is an alias to /src
+
+export default {
+
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "./components/Home.vue";
 
 Vue.use(Router);
 
@@ -10,8 +10,8 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
-    },
-    {
+    }
+    // {
       // path: "/about",
       // name: "about",
       // route level code-splitting
@@ -19,6 +19,6 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       // component: () =>
       //   import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+    // }
   ]
 });
