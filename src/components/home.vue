1229de04e88d1464dfb0f1d1d34f34cf35ffaef1
Askie Lin <isme408@gmail.com>
2019-05-14 18:41:23 +0800

    根據直橫兩種方向渲染首頁的清單呈現

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,75 +1,12 @@
 <template>
   <div class="wholeTask">
-    <div class="taskList">
-      <div class="taskList__description">
-        <div class="taskList__description__word">文</div>
-        <div class="taskList__description__word">橫</div>
-        <div class="taskList__description__word">功</div>
-        <div class="taskList__description__word">能</div>
-        <div class="taskList__description__word">橫</div>
-      </div>
-      <div class="taskList__tasks-type1">
-        <div
-          v-for="n in 15"
-          class="task"
-          :class="{ task__completed: isFinish('M' + n) }"
-          @click="nextPage('M' + n)"
-        >
-          <span>{{ n }}</span>
-        </div>
-      </div>
-      <div class="taskList__tasks-type2">
-        <div
-          v-for="n in 15"
-          class="task"
-          :class="{ task__completed: isFinish('F' + n) }"
-          @click="nextPage('F' + n)"
-        >
-          <span>{{ n }}</span>
-        </div>
-      </div>
-    </div>
-    <!-- <div class="taskList">
-      <div class="taskList__description">
-        <div class="taskList__description__word">文</div>
-        <div class="taskList__description__word">橫</div>
-        <div class="taskList__description__word">功</div>
-        <div class="taskList__description__word">能</div>
-        <div class="taskList__description__word">橫</div>
-      </div>
-      <div class="taskList__tasks-type1">
-        <div class="task task__completed">
-          <span>1</span>
-        </div>
-        <div class="task">
-          <span>2</span>
-        </div>
-        <div class="task">
-          <span>3</span>
-        </div>
-        <div class="task">
-          <span>4</span>
-        </div>
-        <div class="task">
-          <span>5</span>
-        </div>
-      </div>
-      <div class="taskList__tasks-type2">
-        <div class="task task__completed">1</div>
-        <div class="task">
-          <span>2</span>
-        </div>
-        <div class="task">
-          <span>3</span>
-        </div>
-        <div class="task">
-          <span>4</span>
-        </div>
-        <div class="task">
-          <span>5</span>
-        </div>
-      </div>
-    </div> -->
+    <template v-for="(words, wordsIndex) in directions">
+      <taskList
+        v-for="(functions, funIndex) in directions"
+        :directions="{ 'words': words, 'functions': functions, 'listIndex': (wordsIndex * directions.length) + 1 + funIndex }"
+        :key="`${wordsIndex}${funIndex}`"
+      ></taskList>
+    </template>
   </div>
 </template>
 
@@ -78,111 +15,22 @@
   padding: 55px 31px 55px 0;
   font-size: $contentSize;
 }
-
-%task {
-  width: 55px;
-  height: 55px;
-  border-radius: 50%;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-}
-
-.task {
-  @extend %task;
-
-  span {
-    line-height: 1.15em;
-  }
-}
-
-.task:nth-child(2n + 2) span {
-  border-top: 0.1em solid transparent;
-  border-bottom: 0.1em solid $white;
-}
-
-.task {
-  margin-left: 8px;
-  margin-bottom: 10px;
-}
-
-.taskList {
-  background: $gray-2;
-  color: $white;
-  border-radius: 0 30px 30px 0;
-  padding: 18px 29px 7px 15px;
-  display: flex;
-  min-height: 222px;
-  max-width: 706px;
-  margin: 0;
-
-  &__tasks-type1 {
-    .task {
-      background: $normalColor;
-    }
-  }
-
-  &__tasks-type2 {
-    .task {
-      background: $alertColor;
-    }
-  }
-
-  &__tasks-type1,
-  &__tasks-type2 {
-    display: flex;
-    flex-flow: row wrap;
-    flex-grow: 1;
-    flex-shrink: 0;
-    flex-basis: calc((100% - 70px) / 2);
-    max-width: 50%;
-
-    .task__completed {
-      background: $gray-1;
-    }
-  }
-
-  &__description {
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
-    padding-right: 10px;
-  }
-}
-
-.taskList + .taskList {
-  margin-top: 12px;
-}
 </style>
 
 <script>
 // @ is an alias to /src
+import taskList from './taskList.vue';
 
 export default {
+  components: {
+    taskList
+  },
   data() {
     return {
-      finishId: []
+      directions: ['column', 'row']
     };
   },
   methods: {
-    isFinish(id) {
-      let result = false;
-      if ($cookies.get(id) !== null) {
-        console.log($cookies.get(id));
-        result = true;
-      }
-      return result;
-    },
-    nextPage(id) {
-      let result = this.isFinish(id);
-      if (result) {
-        this.$router.push({ name: 'finishTask', params: { id } });
-      } else {
-        this.$router.push({ name: 'login', params: { id } });
-      }
-    }
   }
 };
 </script>
---
+++ src/components/taskList.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="taskList">
+    <div class="taskList__description" v-html="titleTemplate">
+      <!-- {{titleTemplate}} -->
+    </div>
+    <div class="taskList__tasks-type1">
+      <div
+        v-for="n in 15"
+        class="task"
+        :class="{ task__completed: isFinish('M' + n) }"
+        @click="nextPage('M' + n)"
+      >
+        <span>{{ n }}</span>
+      </div>
+    </div>
+    <div class="taskList__tasks-type2">
+      <div
+        v-for="n in 15"
+        class="task"
+        :class="{ task__completed: isFinish('F' + n) }"
+        @click="nextPage('F' + n)"
+      >
+        <span>{{ n }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+%task {
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+.task {
+  @extend %task;
+
+  span {
+    line-height: 1.15em;
+  }
+}
+
+.task:nth-child(2n + 2) span {
+  border-top: 0.1em solid transparent;
+  border-bottom: 0.1em solid $white;
+}
+
+.task {
+  margin-left: 8px;
+  margin-bottom: 10px;
+}
+
+.taskList {
+  background: $gray-2;
+  color: $white;
+  border-radius: 0 30px 30px 0;
+  padding: 18px 29px 7px 15px;
+  display: flex;
+  min-height: 222px;
+  max-width: 706px;
+  margin: 0;
+
+  &__tasks-type1 {
+    .task {
+      background: $normalColor;
+    }
+  }
+
+  &__tasks-type2 {
+    .task {
+      background: $alertColor;
+    }
+  }
+
+  &__tasks-type1,
+  &__tasks-type2 {
+    display: flex;
+    flex-flow: row wrap;
+    flex-grow: 1;
+    flex-shrink: 0;
+    flex-basis: calc((100% - 70px) / 2);
+    max-width: 50%;
+
+    .task__completed {
+      background: $gray-1;
+    }
+  }
+
+  &__description {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    padding-right: 10px;
+  }
+}
+
+.taskList + .taskList {
+  margin-top: 12px;
+}
+</style>
+
+
+<script>
+export default {
+  props: ['directions'],
+  data() {
+    return {
+      finishId: [],
+      titleTemplate: ''
+    };
+  },
+  mounted () {
+    this.$nextTick( () => {
+      console.log(this.directions.words, this.directions.functions, this.directions.listIndex);
+      this.makeTitleTemplate(this.directions.words, this.directions.functions)
+    })
+  },
+  methods: {
+    makeTitleTemplate (directionWords, directionFunctions) {
+      const titleString = 
+        `文${directionToString(directionWords)}功能${directionToString(directionFunctions)}`;
+
+      this.titleTemplate = titleString
+        .split('')
+        .map(title => (`<div class="taskList__description__word">${title}</div>`))
+        .join('');
+
+
+      function directionToString(dir) {
+        return (dir === 'row') ? '直' : '橫'
+      }
+    },
+    isFinish(id) {
+      let result = false;
+      if ($cookies.get(id) !== null) {
+        console.log($cookies.get(id));
+        result = true;
+      }
+      return result;
+    },
+    nextPage(id) {
+      let result = this.isFinish(id);
+      if (result) {
+        this.$router.push({ name: 'finishTask', params: { id } });
+      } else {
+        this.$router.push({ name: 'login', params: { id } });
+      }
+    }
+  }
+}
+</script>
+
